af6e90f3f6a4380d4ebc32cb615f4f7684a38b56
pvictorc <pvictorc@gmail.com>
2024-10-20 20:26:21 -0300

    SRE setup tools script

---
+++ environment/README.md
@@ -0,0 +1,12 @@
+
+# DevOps / SRE environment tools
+
+* Install Python https://www.python.org/downloads/
+* Install kubectl https://kubernetes.io/docs/tasks/tools/
+* Install helm https://helm.sh/docs/intro/install/
+* Install AWS CLI https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
+* Install Terraform https://www.terraform.io/downloads.html
+* Install Postman 
+* (**Optional**) Use Lens to manage your Kubernetes https://k8slens.dev/
+
+Script to install the tools on Ubuntu linux: [install_ubuntu.sh](install_ubuntu.sh)
---
+++ environment/install_ubuntu.sh
@@ -0,0 +1,38 @@
+
+
+# Install Python https://www.python.org/downloads/
+sudo apt-get update
+sudo apt install -y python3
+
+# Install kubectl https://kubernetes.io/docs/tasks/tools/
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+
+# If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install helm https://helm.sh/docs/intro/install/
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+# Install AWS CLI https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
+
+# Install Terraform https://www.terraform.io/downloads.html
+
+
+# Install Lens
+curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
+sudo apt update && sudo apt install lens -y
